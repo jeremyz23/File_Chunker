ace2a4c131f4bb7e3a1320e896144b56fc575799
jeremyz23 <jeremyz23@users.noreply.github.com>
2019-02-22 11:53:49 -0600

    Cleaner version

--- ChunkTextFile.java
+++ ChunkTextFile.java
@@ -1,99 +1,30 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ChunkTextFile {
 
-	private static final String inputFilename = "inputFile.txt";
+    private static final String inputFilename = "inputFile.txt";
 
-	public static void main(String[] args) {
-
-		BufferedReader reader = null;
-
-		BufferedWriter fileWriter = null;
-
-
-		// Create an ArrayList object to hold the lines of input file
-
-		List<String> lines = new ArrayList<String>();
-
-		try {
-			// Creating BufferedReader object to read the input file
-
-			reader = new BufferedReader(new FileReader("src" + "//" + inputFilename));
-
-			// Reading all the lines of input file one by one and adding them into ArrayList
-			String currentLine = reader.readLine();
-
-			while (currentLine != null) {
-				lines.add(currentLine);
-
-				currentLine = reader.readLine();
-
-			}
-			// End of file read.
-
-			final AtomicInteger counter = new AtomicInteger(0);
-			final int size = 10000;
-
-			Collection<List<String>> partitioned = lines.stream()
-					.collect(Collectors.groupingBy(it -> counter.getAndIncrement() / size)).values();
-			
-			//Printing partitions. Each partition will be written to a file.
-			//Testing confirms the partitioning works correctly.
-			partitioned.forEach(System.out::println);
-
-			//Iterate through the Collections and create a file for List<String> object.
-			//Testing confirms the file is created and properly named.
-			Integer count = 0;
-			for (List<String> chunks : partitioned) {
-				// Prepare new incremented file name.
-				String outputFile = "batched_items_file_";
-				String txt = ".txt";
-				 count++;
-
-				String filename = outputFile + count + txt;
-
-				// Write file to directory.
-				fileWriter = new BufferedWriter(new FileWriter("src" + "//" + outputFile));
-				fileWriter = new BufferedWriter(new FileWriter(filename));
-				
-				//Iterate through the List of Strings and write each String to the file.
-				//Writing is not successful. Only 1 file is created and it is empty.
-				for (String chunk : chunks) {
-					// Prepare list of strings to be written to new file.
-					// Write each item number to file.
-				    fileWriter.write(chunk + System.lineSeparator());
-				}
-
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			// Closing the resources
-			System.out.println("Finished");
-
-			try {
-				if (reader != null) {
-					reader.close();
-				}
-
-				if (fileWriter != null) {
-					fileWriter.close();
-				}
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public static void main(String[] args) {
+        final int size = 10000;
+        try(BufferedReader reader=Files.newBufferedReader(Paths.get("src", inputFilename))) {
+            String line = reader.readLine();
+            for(int count = 0; line != null; count++) {
+                try(BufferedWriter writer = Files.newBufferedWriter(
+                        Paths.get("batched_items_file_" + count + ".txt"))) {
+                    for(int i = 0; i < size && line != null; i++) {
+                        writer.write(line);
+                        writer.newLine();
+                        line = reader.readLine();
+                    }
+                }
+            }
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
